d5994d3f0ccd807c76a9bba390ae2d37f589fe43
Theo <asakvsa.idn@gmail.com>
2024-08-15 12:05:10 +0700

    email update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -56,8 +56,8 @@ const About = () => {
 					</span>
 					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-2">
 						<ConnectButton
-							link="vtp@gmail.com"
-							title="vtp@gmail.com"
+							link="valentinustheoprathama@gmail.com"
+							title="valentinustheoprathama@gmail.com"
 							icon={<Mail size={14} />}
 						/>
 						<ConnectButton
