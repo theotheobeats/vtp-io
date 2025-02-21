e9fe758922edd2258c18cad21dd714631bb719d2
Theo <asakvsa.idn@gmail.com>
2025-02-21 14:37:33 +0700

    Add new projects to the portfolio: Nike Landing Page, Multi AI - OSMerge, Apple Landing Page - Clone, and Bankrude

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,23 @@ export const projects = [
 		tags: ["nextjs", "tailwindcss", "supabase", "postgresql", "shadcn"],
 	},
 	{
+		name: "Nike Landing Page",
+		description: "A nike landing design.",
+		image: "/projects/nike_landing_page.png",
+		url: "https://nike-six-nu.vercel.app/",
+		github: "https://github.com/theotheobeats/nike",
+		tags: ["nextjs", "tailwindcss", "shadcn"],
+	},
+	{
+		name: "Multi AI - OSMerge",
+		description:
+			"AI wrapper application (on development) to intergrate multiple AI in one platform.",
+		image: "/projects/multi-ai.png",
+		url: "https://osmerge-development.vercel.app/signin",
+		github: "https://github.com/theotheobeats",
+		tags: ["nextjs", "tailwindcss", "shadcn"],
+	},
+	{
 		name: "Soseal",
 		description:
 			"A social-media clone made using Nextjs, Tailwindcss, shadcn, @tanstack: React Query, Lucia Auth, Prisma ORM and Supabase.",
@@ -32,10 +49,23 @@ export const projects = [
 		image: "/projects/insight-ai.png",
 		url: "https://insight-ai-red.vercel.app/",
 		github: "https://github.com/theotheobeats/insight-ai",
-		tags: [
-			"nextjs",
-			"tailwindcss",
-			"upstash",
-		],
+		tags: ["nextjs", "tailwindcss", "upstash"],
+	},
+	{
+		name: "Apple Landing Page - Clone",
+		description: "Landing page design clone from Apple website",
+		image: "/projects/apple_landing_page.png",
+		url: "https://apple-clone-tau-silk.vercel.app/",
+		github: "https://github.com/theotheobeats/apple_clone",
+		tags: ["nextjs", "tailwindcss", "vite"],
+	},
+	{
+		name: "Bankrude",
+		description:
+			"Banking app project for dashboard cloning from design to code and API intergration exercise.",
+		image: "/projects/bankrude.png",
+		url: "https://bankrude.vercel.app/sign-in",
+		github: "https://github.com/theotheobeats/bankrude",
+		tags: ["nextjs", "tailwindcss", "shadcn"],
 	},
 ];
